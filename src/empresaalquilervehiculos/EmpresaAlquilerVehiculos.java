5d27d36b437cb0c6f5061242326875a68787ee92
Mouad <mouad.maimouni@gmail.com>
2019-01-31 14:24:43 +0100

    Atributos

---
+++ src/empresaalquilervehiculos/EmpresaAlquilerVehiculos.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package empresaalquilervehiculos;
+
+/**
+ *
+ * @author mouad
+ */
+public class EmpresaAlquilerVehiculos {
+
+    // Atributos de la empresa
+    private String cif;
+    private String nombre;
+    private String paginaWeb;
+}
---
+++ src/empresaalquilervehiculos/Vehiculo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package empresaalquilervehiculos;
+
+/**
+ *
+ * @author mouad
+ */
+public class Vehiculo {
+    
+    private int totalVehiculos;
+    private Vehiculo[] vehiculos;
+    
+}
